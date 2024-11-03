7da7192e836bc62b266bcc8c613eec030f3ad323
Gojo <gael1172444@gmail.com>
2024-11-03 16:01:01 -0600

    d

--- ProyectoIntegrador/vistas/inicioSecretario.php
+++ ProyectoIntegrador/vistas/inicioSecretario.php
@@ -11,6 +11,7 @@ if (isset($_SESSION['usuario'])) {
     exit;
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -18,7 +19,7 @@ if (isset($_SESSION['usuario'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Secretario</title>
         <link rel="shortcut icon" href="../img/icono.png">
-        <link rel="stylesheet" href="../css/InicioSecretario.css">
+        <link rel="stylesheet" href="../css/inicioSecretario.css">
         
     </head>
     <body>
