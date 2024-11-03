cfb32cf69121588817e345d17dbadff093e83948
Gojo <gael1172444@gmail.com>
2024-11-03 15:56:19 -0600

    gg

--- ProyectoIntegrador/index.php
+++ ProyectoIntegrador/index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Comité Académico</title>
-        <link rel="stylesheet" href="../css/Index.css">
+        <link rel="stylesheet" href="css/Index.css">
         <link rel="shortcut icon" href="img/icono.png">
     </head>
     <script>
